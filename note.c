7cb4db95c4f1fd0a4264e7ec21edb4af5fb3a74d
akbar <akbartangir99@gmail.com>
2023-04-05 10:25:25 +0500

    edit note

--- note.c
+++ note.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 
-int main(void)
+void foo1(int xval)
 {
-    printf("Hello, world!\n");
-    printf("I am a C program.\n");
-    
+    int x;
+    x = xval;
+    printf("foo1: x address is %p \n", &x);
+    printf("foo1: x value is %d \n\n", x);
+}
+
+void foo2(int yval)
+{
+    int y;
+    printf("foo2: y address is %p \n", &y);
+    printf("foo2: y value is %d \n", y);
+}
+
+int main()
+{
+    foo1(7);
+    foo2(11);
     return 0;
 }
 
 
+/*
+    foo1: x address is 0x7fffd2612794
+    foo1: x value is 7
 
-
-
-
-
+    foo2: y address is 0x7fffd2612794 
+    foo2: y value is 7
+*/
