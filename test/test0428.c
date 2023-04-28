b45846f7cb9a91dda8bdbea6ba2d2f9235b88d54
akbar <akbartangir99@gmail.com>
2023-04-28 21:48:44 +0500

    Lab problems 04/28/2023

---
+++ test/test0428.c
@@ -0,0 +1,41 @@
+/*
+    CopyRight: Akbar Tangirov (E30C)
+    Ajou University in Tashkent Electrical and Computer Engineering Department
+    date: 04/28/2023
+    description: Test problems
+*/
+#include <stdio.h>
+#include <math.h>
+
+float fsin(float x)
+{
+	return sin(x);
+}
+
+void farr(int x[])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%d\n", x[i]);
+	}
+}
+
+int main(void)
+{
+	int i, sum = 0, A[] = {9, 19, 99};
+
+	printf("name: Akbar (E30C)\n\n");
+
+	for (i = 2; i < 21; i+=2)
+	{
+		printf("%d  ", i);
+		sum = sum + i;
+	}
+	printf("sum: %d\n\n", sum);
+
+	printf("sin(pi) = %f\n\n", fsin(M_PI));
+
+	farr(A);
+
+
+}
