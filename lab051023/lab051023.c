e7f467d9c7b67d247c3d6494c2a0c823f98c56b2
akbarajou <202290398@ajou.uz>
2023-05-11 09:11:13 +0500

    commit

--- lab051023/lab051023.c
+++ lab051023/lab051023.c
@@ -1,46 +1,48 @@
 #include <stdio.h>
 
-void name(void)
-{
-	printf("name: Akbar (E30C)\n");
+// Define constants for array sizes
+#define SIZE_A 4
+#define SIZE_S 6
+
+// Print name and class information
+void print_name(void){
+    printf("name: Akbar (E30C)\n");
 }
 
-
-int f_arr(int *x)
-{
-	for(int i = 0; i <= 4; i++)
-		x[i] *= 2;
+// Double the elements of an integer array
+void double_array_elements(int *x, int size){
+    for(int i = 0; i < size; i++)
+        x[i] *= 2;
 }
 
+int main(void){
+    int i, num, sum = 0;
+    int A[SIZE_A] = {1, 2, 3, 4};
+    char S[SIZE_S] = "banana";
 
-int main(void)
-{
-	int i, num, sum = 0, A[] = {1, 2, 3, 4};
-	char S[] = "banana";
+    // First solution: print name and class information
+    print_name();
 
-	// first solution
-	name();
+    // Second solution: read an integer from user input and print it
+    printf("Enter integer number: ");
+    scanf("%d", &num);
+    printf("num: %d\n", num);
 
-	// second solution 
-	printf("Enter integer number: ");
-	scanf("%d", &num);
-	printf("num: %d\n", num);
+    // Third solution: calculate and print the sum of multiples of 3 from 3 to 30
+    for (i = 3; i <= 30; i += 3)
+        sum += i;
+    printf("sum: %d\n", sum);
 
-	// third solution
-	for (i = 3; i <= 30; i += 3)
-		sum += i;
-	printf("sum: %d\n", sum);
+    // Fourth solution: print a string and its ASCII codes
+    printf("%s\n", S);
+    for (i = 0; i < SIZE_S; i++)
+        printf("ASCII code %c is %d\n", S[i], S[i]);
 
-	// fourth solution
-	printf("%s\n", S);
-	for (i = 0; i < 6; i++)
-		printf("ASCII code %c is %d\n", S[i], S[i]);
+    // Fifth solution: double the elements of an integer array and print them
+    double_array_elements(A, SIZE_A);
+    for (i = 0; i < SIZE_A; i++)
+        printf("%d ", A[i]);
+    printf("\n");
 
-	// fifth solution
-	f_arr(A);
-	for (i = 0; i < 4; i++)
-		printf("%d	", A[i]);
-	printf("\n");
-
-	return 0;
+    return 0;
 }
